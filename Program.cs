65d4a1bcabeab5a4efcf1d4ec090e6f3704175ac
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2021-01-11 20:43:06 +0530

    First Tine Using .NET

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Yuvraj Dagur";
+            Console.WriteLine($"Hello World! I am {name}");
+            Console.WriteLine("This is the first time I'm using .Net");
+        }
+    }
+}
